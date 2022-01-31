9d2febf2a141895db46f0345c9c0143b3ebc2724
kebekchala <chalakebek@gmail.com>
2022-01-31 23:53:06 +0300

    in

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,16 @@ import firebase from "firebase";
 import "firebase/auth"
 
 var app = firebase.initializeApp({
-    apiKey: "AIzaSyAEHgv8QgL-nHdpAPhh6Ckf4N-8n6aPqmU",
-    authDomain: "hakim-40c01.firebaseapp.com",
-    projectId: "hakim-40c01",
-    storageBucket: "hakim-40c01.appspot.com",
-    messagingSenderId: "460178294634",
-    appId: "1:460178294634:web:fc2f3184219ad87630c713",
-    measurementId: "G-81VEHBR9D1"
+    
+    apiKey:process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain:process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId:process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket:process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId:process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId:process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+
+
 })
 
 export default app;
